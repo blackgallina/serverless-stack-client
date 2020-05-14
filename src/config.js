c7857aef8ce3bb6096951dcf83be5f5c02c67a65
blackgallina <blackgallina@gmail.com>
2020-05-14 11:01:31 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "democarlos-notesappuploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://gqferc2j48.execute-api.eu-west-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_heVQhLo6a",
+    APP_CLIENT_ID: "69jr65q065u5c890ud0eg4kovr",
+    IDENTITY_POOL_ID: "eu-west-1:2728e83c-8368-4699-b798-19414cfca012"
+  }
+};
